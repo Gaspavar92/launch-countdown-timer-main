17a860b99816c383a08abf26aef0652ff89e83e2
Gaspare Tortora <g.tortora2@gmail.com>
2023-03-21 20:42:06 -0400

    Refactored JavaScript functions to shorten code

--- script.js
+++ script.js
@@ -6,24 +6,6 @@ const hoursHtml = document.querySelector(".time2")
 const minutesHtml = document.querySelector(".time3")
 const secondsHtml = document.querySelector(".time4")
 
-
-// Getting current date and launch date to set up initial value
-
-let launchDate = new Date("April 3, 2023 08:00:00").getTime();
-let currentDate = new Date().getTime();
-let distance = launchDate - currentDate;
-
-let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-daysHtml.textContent = days;
-hoursHtml.textContent = hours;
-minutesHtml.textContent = minutes;
-secondsHtml.textContent = seconds;
-
-
 // Creating a function to calculate the time
 
 const calculateTimeLeft = () => {
@@ -45,6 +27,10 @@ const calculateTimeLeft = () => {
     secondsHtml.textContent = seconds;
 
 }
+
+// Getting current date and launch date to set up initial value
+
+calculateTimeLeft();
 
 //Creating a function to update the time value every second
 
