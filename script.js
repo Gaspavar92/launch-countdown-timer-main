c4a858eba833de2e72b4d3bb834405e0c7f412cf
Gaspare Tortora <g.tortora2@gmail.com>
2023-03-30 11:44:02 -0400

    Added flip card functionality

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const hoursHtml = document.querySelector(".time2")
 const minutesHtml = document.querySelector(".time3")
 const secondsHtml = document.querySelector(".time4")
 
+const daysHtml2 = document.querySelector(".time5")
+const hoursHtml2 = document.querySelector(".time6")
+const minutesHtml2 = document.querySelector(".time7")
+const secondsHtml2 = document.querySelector(".time8")
+
+const daysCard = document.querySelector(".days");
+const hoursCard = document.querySelector(".hours");
+const minutesCard = document.querySelector(".minutes");
+const secondsCard = document.querySelector(".seconds");
+
 // Creating a function to calculate the time
 
 const calculateTimeLeft = () => {
@@ -26,6 +36,11 @@ const calculateTimeLeft = () => {
     minutesHtml.textContent = minutes;
     secondsHtml.textContent = seconds;
 
+    daysHtml2.textContent = days;
+    hoursHtml2.textContent = hours;
+    minutesHtml2.textContent = minutes;
+    secondsHtml2.textContent = seconds;
+
 }
 
 // Getting current date and launch date to set up initial value
@@ -34,4 +49,22 @@ calculateTimeLeft();
 
 //Creating a function to update the time value every second
 
-setInterval(calculateTimeLeft, 1000)
+setInterval(calculateTimeLeft, 1000);
+
+// Creating a function that will flip the card every time a change is detected
+
+secondsCard.addEventListener("click", () => {
+    secondsCard.classList.toggle("is-flipped")
+})
+
+minutesCard.addEventListener("click", () => {
+    minutesCard.classList.toggle("is-flipped")
+})
+
+hoursCard.addEventListener("click", () => {
+    hoursCard.classList.toggle("is-flipped")
+})
+
+daysCard.addEventListener("click", () => {
+    daysCard.classList.toggle("is-flipped")
+})
